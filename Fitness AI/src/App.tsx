e4d012c2f9eb65d1bff30007d9c06f5747e43842
Khalidsaadhabash <a0500394433@gmail.com>
2023-09-11 01:00:49 +0300

    Api and bmi

--- Fitness AI/src/App.tsx
+++ Fitness AI/src/App.tsx
@@ -4,25 +4,44 @@ import ChatRoom from "./pages/ChatRoom";
 import LoginChat from "./pages/LoginChat";
 import { PrivateRoute } from "./routes/PrivateRoute";
 import { AuthProvider } from "./context/AuthContext";
+import ExerciseDete from "./pages/ExerciseDete";
+import Calculator from "./pages/Calculator";
 
 const App: React.FC = () => {
   return (
-    <AuthProvider>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<LoginChat />} />
-        <Route
-          path="/chat"
-          element={
-            <PrivateRoute>
-              <ChatRoom />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
-      </AuthProvider>
+    <>
+    {/* <ExerciseDete/> */}
+    
+    <Calculator/>
+    
+    
+    
+    </>
+    // <AuthProvider>
+    //   <Navbar />
+    //   <Routes>
+    //     <Route path="/" element={<LoginChat />} />
+    //     <Route
+    //       path="/chat"
+    //       element={
+    //         <PrivateRoute>
+    //           <ChatRoom />
+    //         </PrivateRoute>
+    //       }
+    //     />
+    //   </Routes>
+    //   </AuthProvider>
       
   );
 };
 
 export default App;
+
+
+          
+ 
+    
+   
+          
+            
+          
---
+++ Fitness AI/src/pages/ExerciseCardProps .tsx
@@ -0,0 +1,20 @@
+// import React from 'react';
+// import ExerciseDete from  './ExerciseDete'
+// interface ExerciseCardProps {
+//   exercise: Exercise;
+// }
+
+// const ExerciseCard: React.FC<ExerciseCardProps> = ({ exercise }) => {
+//   const { name, description, bodyPart, gifUrl } = exercise;
+
+//   return (
+//     <div className="bg-white p-4 shadow-md rounded-md">
+//       <img src={gifUrl} alt={name} className="w-full h-40 object-cover mb-4" />
+//       <h2 className="text-lg font-bold mb-2">{name}</h2>
+//       <p className="text-gray-600 mb-2">{bodyPart}</p>
+//       <p className="text-gray-800">{description}</p>
+//     </div>
+//   );
+// };
+
+// export default ExerciseCard;
